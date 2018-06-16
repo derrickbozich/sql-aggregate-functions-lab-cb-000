10abd1bbdd73d59bbb9469ee980d0fb1881ec85c
derrick b <dbozich1@gmail.com>
2018-06-16 16:49:29 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,17 +5,17 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
-  'SELECT MIN(net_worth) FROM students;'
+  'SELECT MIN(gpa) FROM students;'
 end
 
 def average_student_gpa
-  'SELECT AVG(net_worth) FROM students;'
+  'SELECT AVG(gpa) FROM students;'
 end
 
 def total_tardies_for_all_students
-  'SELECT SUM(net_worth) FROM students;'
+  'SELECT SUM(tardies) FROM students;'
 end
 
 def average_gpa_for_9th_grade
-  'SELECT AVG(net_worth) FROM students;'
+  'SELECT AVG(gpa) FROM students;'
 end
